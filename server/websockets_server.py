b6ce2ad4eb78956cf5aac8455de44cc26905a809
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2025-06-01 21:04:03 +0200

    Fix SYNC server

--- server/websockets_server.py
+++ server/websockets_server.py
@@ -96,8 +96,8 @@ async def main():
     readLedsSuccess = False
     try:
         # Force keyboard wakeup to prevent infinite blocking while reading led status
-        keyboard.press([], KeyCodes["KEY_NUMLOCK"], release=True)
-        keyboard.press([], KeyCodes["KEY_NUMLOCK"], release=True)
+        keyboard.press([], KeyCodes["KEY_CAPSLOCK"], release=True)
+        keyboard.press([], KeyCodes["KEY_CAPSLOCK"], release=True)
 
         leds = await asyncio.wait_for(
             asyncio.to_thread(keyboard.blocking_read_led_status),
