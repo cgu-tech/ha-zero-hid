70ba25e4d94658a311427fdf1a5fba6f4fc50153
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2025-07-13 14:18:46 +0200

    Fix remote layout

--- client/www/android-remote-card.js
+++ client/www/android-remote-card.js
@@ -343,18 +343,12 @@ class AndroidRemoteCard extends HTMLElement {
     const style = document.createElement("style");
     style.textContent = `
       :host {
-        display: block;
-        width: 100%;
-        box-sizing: border-box;
-      }
-
-      body {
         --base-font-size: 1rem; /* base scaling unit */
         font-size: var(--base-font-size);
         font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
         margin: 0;
       }
-      
+
       .wrapper {
         display: flex;
         flex-direction: column;
