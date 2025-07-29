4f4774f18582a4c4e3da64bc16d0817f628f1a73
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2025-07-29 17:35:09 +0200

    Enhance carrousel

--- client/web/carrousel-card.js
+++ client/web/carrousel-card.js
@@ -21,6 +21,10 @@ class CarrouselCard extends HTMLElement {
     this.cellsWidth = 60;
     this.cellsHeight = 60;
     this.cells = [];
+
+    this.cellImageModes = new Set(["img", "image", "ico", "icon", "pic", "picture", "photo"]);
+    this.cellLabelModes = new Set(["txt", "text", "lbl", "label", "name"]);
+    this.cellMixedModes = new Set(["mix", "mixed", "both", "all"]);
     
     // Layout loading flags
     this._layoutReady = false;
@@ -175,8 +179,15 @@ class CarrouselCard extends HTMLElement {
       .carrousel-cell img {
         width: 90%;
         height: 90%;
-        border-radius: 4px;
         object-fit: contain;
+      }
+      
+      .carrousel-cell img.img-half {
+        border-radius: 4px 4px 0 0; /* top-left, top-right, bottom-right, bottom-left */
+      }
+      
+      .carrousel-cell img.img-full {
+        border-radius: 4px;
       }
 
       .carrousel-label {
@@ -185,10 +196,17 @@ class CarrouselCard extends HTMLElement {
         align-items: center;     /* Vertical centering */
         width: 90%;
         height: 90%;
-        border-radius: 4px;
         white-space: normal;       /* allows wrapping */
         word-wrap: break-word;     /* allows breaking long words */
         overflow-wrap: break-word; /* better support for word breaking */
+      }
+      
+      .carrousel-label.label-half {
+        border-radius: 0 0 4px 4px; /* top-left, top-right, bottom-right, bottom-left */
+      }
+      
+      .carrousel-label.label-full {
+        border-radius: 4px;
       }
     `;
     this.shadowRoot.appendChild(style);
@@ -197,37 +215,42 @@ class CarrouselCard extends HTMLElement {
     container.className = "carrousel-container";
 
     Object.entries(this.cells).forEach(([id, cell]) => {
+      // Create a new cell
       const cellDiv = document.createElement("div");
       cellDiv.className = "carrousel-cell";
       cellDiv.id = id;
-
-      const cellBackgroundColor = cell["background-color"];
-      const cellIconUrl = cell["icon-url"];
-      if (cellIconUrl) {
-        const img = document.createElement("img");
-        img.className = "carrousel-img";
-        const targetCellIconUrl = this.isValidUrl(cellIconUrl) ? cellIconUrl : this.getLocalIconUrl(cellIconUrl);
-        img.addEventListener('error', () => {
-          if (this.logger.isWarnEnabled()) console.warn(...this.logger.warn(`Unable to load image URL:${targetCellIconUrl} for cell:`, (cell.name || id)));
-
-          // Hide image
-          img.style.display = 'none';
-
-          // Create and display alternative label
-          const cellDiv = img.parentElement;
-          const label = document.createElement("div");
-          label.className = "carrousel-label";
-          label.textContent = cell.name || id;
-          if (cellBackgroundColor) label.style.backgroundColor = cellBackgroundColor;
-          cellDiv.appendChild(label);
-        });
-        img.src = targetCellIconUrl;
-        img.alt = cell.name || id;
-
-        if (cellBackgroundColor) img.style.backgroundColor = cellBackgroundColor;
-        cellDiv.appendChild(img);
-      }
       cellDiv._keyData = { config: cell };
+
+      // Retrieve cell user configurations
+      const cellDisplayMode = cell["display-mode"];
+      const cellIconUrl = cell["icon-url"];
+      const cellBackgroundColor = cell["background-color"];
+      const cellNameSize = cell["name-size"];
+      const targetDisplayMode = this.getDisplayMode(cellDisplayMode);
+      
+      // Set cell content (image, label)
+      if (targetDisplayMode === "image") {
+        // Image mode
+        const img = this.createImage(id, cell.name, cellIconUrl, cellBackgroundColor, cellNameSize);
+        img.classList.add('img-full');
+        cellDiv.appendChild(img);
+
+      } else if (targetDisplayMode === "name") {
+        // Label mode
+        const label = this.createLabel(cellId, cellName, cellBackgroundColor, cellNameSize);
+        label.classList.add('label-full');
+        cellDiv.appendChild(label);
+
+      } else {
+        // Image and label mode
+        const img = this.createImage(id, cell.name, cellIconUrl, cellBackgroundColor, cellNameSize);
+        img.classList.add('img-half');
+        const label = this.createLabel(cellId, cellName, cellBackgroundColor, cellNameSize);
+        label.classList.add('label-half');
+        cellDiv.appendChild(img);
+        cellDiv.appendChild(label);
+
+      }
       
       this.addPointerClickListener(cellDiv, (e) => {
         this.handlePointerClick(e, hass, cellDiv);
@@ -238,6 +261,49 @@ class CarrouselCard extends HTMLElement {
 
     card.appendChild(container);
     this.shadowRoot.appendChild(card);
+  }
+  
+  createImage(cellId, cellName, cellIconUrl, cellBackgroundColor, cellNameSize) {
+    const img = document.createElement("img");
+    img.className = "carrousel-img";
+    const targetCellIconUrl = cellIconUrl ? (this.isValidUrl(cellIconUrl) ? cellIconUrl : this.getLocalIconUrl(cellIconUrl)) : "";
+    img.addEventListener('error', () => {
+      // When image sources fails to load
+      if (this.logger.isWarnEnabled()) console.warn(...this.logger.warn(`Unable to load image URL:${targetCellIconUrl} for cell:`, (cellName || cellId)));
+    
+      // Hide image
+      img.style.display = 'none';
+    
+      // Retrieve label with "Name" text as alternative
+      const cellDiv = img.parentElement;
+      let label = cellDiv.querySelector('.carrousel-label');
+      
+      // When missing label (because displayMode was set to image for example)
+      if (!label) {
+    
+        // Create a new label inside the cell and display it
+        label = this.createLabel(cellId, cellName, cellBackgroundColor, cellNameSize);
+        cellDiv.appendChild(label);
+      }
+      
+      // Set the "full" class for the label (to make all rounded corners for example)
+      label.classList.remove('label-half');
+      label.classList.add('label-full');
+    });
+    img.src = targetCellIconUrl;
+    img.alt = cellName || cellId;
+    
+    if (cellBackgroundColor) img.style.backgroundColor = cellBackgroundColor;
+    return img;
+  }
+
+  createLabel(cellId, cellName, cellBackgroundColor, cellNameSize) {
+    const label = document.createElement("div");
+    label.className = "carrousel-label";
+    label.textContent = cellName || cellId;
+    if (cellBackgroundColor) label.style.backgroundColor = cellBackgroundColor;
+    if (cellNameSize && this.isFiniteNumber(cellNameSize) && Number(value) > 0) label.style.fontSize = cellNameSize + 'px';
+    return label;
   }
 
   handlePointerClick(evt, hass, cell) {
@@ -258,11 +324,56 @@ class CarrouselCard extends HTMLElement {
     this.hapticFeedback();
   }
 
+  getDisplayMode(cellDisplayMode) {
+    let targetDisplayMode = null;
+    if (cellDisplayMode) {
+      // Lowerize for equality comparizons
+      let loweredDisplayMode = cellDisplayMode.toLowerCase();
+      if (this.cellImageModes.has(loweredDisplayMode)) {
+        targetDisplayMode = "image";
+      } else if (this.cellLabelModes.has(loweredDisplayMode)) {
+        targetDisplayMode = "name";
+      } else if (this.cellMixedModes.has(loweredDisplayMode)) {
+        targetDisplayMode = "mixed";
+      } else {
+        const regex = /^\s*([^\s]+)\s*-\s*([^\s]+)\s*$/;
+        const match = loweredDisplayMode.match(regex);
+        if (match) {
+          const displayModeOne = match[1];
+          const displayModeTwo = match[2];
+
+          // Both labels matches one then the other display mode: mixed mode
+          if ((this.cellImageModes.has(displayModeOne) && this.cellLabelModes.has(displayModeTwo) ||
+              (this.cellLabelModes.has(displayModeOne) && this.cellImageModes.has(displayModeTwo)) {
+            targetDisplayMode = "mixed";
+          }
+        }
+      }
+      
+      // Invalid cellDisplayMode specified by user: warn it
+      if (targetDisplayMode) {
+        if (this.logger.isWarnEnabled()) console.warn(...this.logger.warn(`Unknown display mode ${cellDisplayMode}: defaulting to 'image'`));
+        targetDisplayMode = "image";
+      }
+    }
+    
+    // No cellDisplayMode specified by user: defaulting silently
+    if (targetDisplayMode) {
+      if (this.logger.isTraceEnabled()) console.debug(...this.logger.trace(`No display mode provided: defaulting to 'image'`));
+      targetDisplayMode = "image";
+    }
+  }
+
+  isFiniteNumber(value) {
+    return Number.isFinite(Number(value));
+  }
+
   getLocalIconUrl(str) {
     return `${Globals.DIR_ICONS}/${str}`;
   }
 
   isValidUrl(str) {
+    if (!str) return false;
     try {
       new URL(str);
       return true;
