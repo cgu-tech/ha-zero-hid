4ca32f270011adf6ca647dec133feee58816a74c
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2025-08-07 17:30:57 +0200

    Add debugging capabilities

--- client/web/android-remote-card.js
+++ client/web/android-remote-card.js
@@ -406,11 +406,11 @@ class AndroidRemoteCard extends HTMLElement {
     }
 
     // Update logger when needed
-    if (this.logger.isTraceEnabled()) console.debug(...this.logger.trace("oldLoglevel, this.loglevel, oldLogpushback, this.logpushback):", oldLoglevel, this.loglevel, oldLogpushback, this.logpushback));
     if (!oldLoglevel || oldLoglevel !== this.loglevel || !oldLogpushback || oldLogpushback !== this.logpushback) {
-      if (this.logger.isTraceEnabled()) console.debug(...this.logger.trace("this.logger.update(this.loglevel, this._hass, this.logpushback):", this.loglevel, this._hass, this.logpushback));
       this.logger.update(this.loglevel, this._hass, this.logpushback);
+      if (this.logger.isTraceEnabled()) console.debug(...this.logger.trace("this.logger.update(this.loglevel, this._hass, this.logpushback):", this.loglevel, this._hass, this.logpushback));
     }
+    if (this.logger.isTraceEnabled()) console.debug(...this.logger.trace("oldLoglevel, this.loglevel, oldLogpushback, this.logpushback:", oldLoglevel, this.loglevel, oldLogpushback, this.logpushback));
     if (this.logger.isDebugEnabled()) console.debug(...this.logger.debug("setConfig(config):", this.config));
 
     // Set haptic feedback
