cbe023060836c1647c0d5a7a788c934e1f788b01
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2025-08-13 12:04:26 +0200

    Refactoring keyboard

--- client/web/android-keyboard-card.js
+++ client/web/android-keyboard-card.js
@@ -199,7 +199,7 @@ class AndroidKeyboardCard extends HTMLElement {
         --key-special-color: #ccc;
         --key-height: clamp(1px, 7vw, 56px);
         --key-margin: 0.15rem;
-        font-size: var(--base-font-size);
+        font-size: clamp(1px, var(--base-font-size), var(--base-font-size));
         display: block;
         width: 100%;
         user-select: none;
@@ -233,7 +233,7 @@ class AndroidKeyboardCard extends HTMLElement {
         border-radius: clamp(1px, 1vw, var(--key-max-corner-radius));
         flex: 1 1 0;
         min-width: 0;      /* prevent content from forcing expansion */
-        font-size: clamp(1px, 1rem, 1rem);
+        font-size: clamp(1px, 1em, 1em);
         padding: clamp(1px, 0.6vw, 4px); clamp(1px, 0.6vw, 4px);
         cursor: pointer;
         height: var(--key-height);
@@ -253,7 +253,7 @@ class AndroidKeyboardCard extends HTMLElement {
       }
       button.key.special {
         flex: 1 1 auto;
-        font-size: clamp(1px, 0.95rem, 0.95rem);
+        font-size: clamp(1px, 0.95em, 0.95em);
         font-weight: bold;
         padding: clamp(1px, 0.6vw, 4px); clamp(0px, 0.3vw, 2px);
         background: var(--key-special-bg);
