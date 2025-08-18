c81c79124b4f3074ea69d9bcc050c56c063de838
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2025-08-18 18:54:41 +0200

    Fixing layouts

--- client/web/windows-keyboard-card.js
+++ client/web/windows-keyboard-card.js
@@ -249,7 +249,7 @@ export class WindowsKeyboardCard extends HTMLElement {
         width: 100%;
         box-sizing: border-box;
         flex-shrink: 1;
-        gap: clamp(1px, 1vw, 6px);
+        gap: clamp(3px, 1vw, 6px);
       }
       button.key {
         border-radius: clamp(var(--key-min-corner-radius), 1vw, var(--key-max-corner-radius));
@@ -330,13 +330,16 @@ export class WindowsKeyboardCard extends HTMLElement {
       button.key.shift-once.${this._eventManager.constructor._BUTTON_CLASS_PRESSED} { 
         background: var(--key-shif-once-press-bg);
       }
+      button.key.special.locked,
       button.key.locked {
         background: var(--key-locked-bg);
         font-weight: bold;
       }
+      button.key.special.locked.${this._eventManager.constructor._BUTTON_CLASS_HOVER},
       button.key.locked.${this._eventManager.constructor._BUTTON_CLASS_HOVER} {
         background: var(--key-locked-active-bg);
       }
+      button.key.special.locked.${this._eventManager.constructor._BUTTON_CLASS_PRESSED},
       button.key.locked.${this._eventManager.constructor._BUTTON_CLASS_PRESSED} {
         background: var(--key-locked-press-bg);
       }
