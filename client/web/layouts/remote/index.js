d3b61fec8d9223a864c38a023e8b0cbed593e08b
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2025-08-21 00:23:57 +0200

    Fixing layouts

--- client/web/layouts/remote/index.js
+++ client/web/layouts/remote/index.js
@@ -1,3 +1,4 @@
 export { layoutRemoteClassic } from './classic.js';
 export { layoutRemoteRightHandV1 } from './right-hand-v1.js';
 export { layoutRemoteRightHandV2 } from './right-hand-v2.js';
+export { layoutRemoteRightHandV3 } from './right-hand-v3.js';
