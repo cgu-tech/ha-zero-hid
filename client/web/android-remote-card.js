9a2325acb2b5d785100d079c63ce503d9f005f48
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2025-08-22 22:13:57 +0200

    Fixing layouts

--- client/web/android-remote-card.js
+++ client/web/android-remote-card.js
@@ -225,10 +225,10 @@ class AndroidRemoteCard extends HTMLElement {
         box-sizing: border-box;
       }
       .wrapper.with-addons {
-        width: 85.7142%; /* 6/7 */
+        width: 83,3333%; /* 5/6 */
       }
       .addons-wrapper {
-        width: 14.2858%; /* 1/7 */
+        width: 16,6667%; /* 1/6 */
         position: absolute;
         top: 0;
         right: 0;
