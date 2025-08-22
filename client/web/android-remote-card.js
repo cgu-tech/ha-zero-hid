d5b78713ea6c4ffef5110ab61d990aa727be8941
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2025-08-22 22:21:08 +0200

    Fixing layouts

--- client/web/android-remote-card.js
+++ client/web/android-remote-card.js
@@ -258,7 +258,7 @@ class AndroidRemoteCard extends HTMLElement {
         align-items: center;
         justify-content: center;
         display: flex;
-        border-radius: var(--card-border-radius);
+        border-radius: 50%;
         margin-top: 4px;
         margin-bottom: 0px;
         margin-left: 4px;
