414d5a1d8c84c0d725bda694a05e4e8018a829cf
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2025-08-22 18:42:42 +0200

    Fixing layouts

--- client/web/android-remote-card.js
+++ client/web/android-remote-card.js
@@ -247,6 +247,7 @@ class AndroidRemoteCard extends HTMLElement {
         justify-content: flex-start; /* Avoid vertical stretching */
         overflow-y: auto; /* Scroll if content overflows */
         min-width: 0;
+        min-height: 0; /* allows scrolling inside */
         max-height: 100%;
       }
       .hide {
