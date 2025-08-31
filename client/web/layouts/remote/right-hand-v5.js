c59232643ab87ddd78b855761f4e6013a9e20aff
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2025-08-31 12:33:23 +0200

    Add air-mouse capabilities

---
+++ client/web/layouts/remote/right-hand-v5.js
@@ -0,0 +1,57 @@
+export const layoutRemoteRightHandV5 = {
+    "Name": "right-hand-v5",
+    "Description": "Contains extended right-handed layout for android remote buttons",
+    "rows": [
+      {
+        "cells": [
+          { "name": "remote-button-power-tv",       "weight": 1 },
+          { "name": "remote-button-power-shield-1", "weight": 1 },
+          { "name": "remote-button-power-device",   "weight": 1 },
+          { "name": "remote-button-settings",       "weight": 1 },
+          { "name": "remote-button-bulb",           "weight": 1 },
+        ]
+      },
+      {
+        "filler-top": 1,
+        "cells": [
+          { "name": "filler",                       "weight": 0.5 },
+          { "name": "dpad",                         "weight": 4 },
+          { "name": "filler",                       "weight": 0.5 }
+        ]
+      },
+      {
+        "filler-top": 1,
+        "filler-bottom": 1,
+        "cells": [
+          { "name": "filler"              ,         "weight": 0.5 },
+          { "name": "remote-button-return",         "weight": 2   },
+          { "name": "remote-button-home"  ,         "weight": 2   },
+          { "name": "filler"              ,         "weight": 0.5 }
+        ]
+      },
+      {
+        "filler-top": 1,
+        "filler-bottom": 1,
+        "cells": [
+          { "name": "remote-button-backspace",      "weight": 1 },
+          { "name": "ts-toggle-container",          "weight": 3 },
+          { "name": "remote-button-air-mouse",      "weight": 1 },
+        ]
+      },
+      {
+        "no-gap": true,
+        "cells": [
+          { "name": "foldable-container",           "weight": 5 }
+        ]
+      },
+      {
+        "cells": [
+          { "name": "remote-button-play-pause",     "weight": 1 },
+          { "name": "remote-button-hid-server",     "weight": 1 },
+          { "name": "remote-button-volume-mute",    "weight": 1 },
+          { "name": "remote-button-volume-down",    "weight": 1 },
+          { "name": "remote-button-volume-up",      "weight": 1 }
+        ]
+      }
+    ]
+}
