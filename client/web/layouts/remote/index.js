595d89fbc80b49ea3390334722c0a85e0f7a7fbf
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2025-08-31 12:33:47 +0200

    Add air-mouse capabilities

--- client/web/layouts/remote/index.js
+++ client/web/layouts/remote/index.js
@@ -3,3 +3,4 @@ export { layoutRemoteRightHandV1 } from './right-hand-v1.js';
 export { layoutRemoteRightHandV2 } from './right-hand-v2.js';
 export { layoutRemoteRightHandV3 } from './right-hand-v3.js';
 export { layoutRemoteRightHandV4 } from './right-hand-v4.js';
+export { layoutRemoteRightHandV5 } from './right-hand-v5.js';
