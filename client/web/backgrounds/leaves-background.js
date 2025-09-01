e1ed08a1c1f71e4172680a5560db7045de6eaea2
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2025-09-01 20:19:58 +0200

    Add air-mouse capabilities

--- client/web/backgrounds/leaves-background.js
+++ client/web/backgrounds/leaves-background.js
@@ -82,6 +82,7 @@ export class FallingLeavesBackground extends HTMLElement {
     const createLeaf = () => {
       const g = document.createElementNS("http://www.w3.org/2000/svg", "g");
       g.classList.add("leaf");
+      g.style.visibility = 'hidden'; // Hide until animation begins
 
       const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
 
@@ -107,7 +108,6 @@ export class FallingLeavesBackground extends HTMLElement {
       const rotateEnd = rotateStart + rand(90, 360);
 
       leaf.setAttribute("transform", `translate(${startX}, -60)`);
-      leaf.style.visibility = 'hidden'; // Hide until animation begins
 
       const animation = leaf.animate([
         {
@@ -128,9 +128,9 @@ export class FallingLeavesBackground extends HTMLElement {
       });
 
       // Show the leaf once animation actually starts
-      animation.onready = () => {
+      animation.ready.then(() => {
         leaf.style.visibility = 'visible';
-      };
+      });
 
       animation.onfinish = () => {
         animation.cancel();
