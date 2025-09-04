f00b3a3aab1480f75b0d597a488dc2524f81c12b
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2025-09-04 19:16:33 +0200

    Add air-mouse capabilities

--- client/web/backgrounds/falling-background.js
+++ client/web/backgrounds/falling-background.js
@@ -118,7 +118,7 @@ export class FallingBackground extends HTMLElement {
   }
 
   createFalling() {
-    return this.createFallingSpider();
+    return this.createFallingPumkin();
   }
 
   setRandomOpacity(elt, min, max) {
@@ -209,7 +209,7 @@ export class FallingBackground extends HTMLElement {
       c-0.7-0.4-1.3-1-1.7-1.7l-1.3-2.6c-0.1-0.3-0.4-0.5-0.7-0.5l-2.1-0.4l2.7-1.4l0.1,0.1C26,16.4,27.5,17,29,17c0.6,0,1-0.4,1-1
       S29.6,15,29,15z`);
 
-    this.setRandomFill(path, ['#000000', '#1C1C1C', '#262626']);
+    this.setRandomFill(path, ['#000000']);
     this.setRandomOpacity(path, 0.6, 1);
     this.setRandomScale(path, 0.8, 1.8);
 
@@ -218,6 +218,35 @@ export class FallingBackground extends HTMLElement {
     return falling;
   }
 
+  createFallingPumkin() { 
+    // Create the <g> wrapper for the spider
+    const falling = document.createElementNS("http://www.w3.org/2000/svg", "g");
+    falling.classList.add("falling"); // Optional: for animation or styling
+    falling.style.visibility = "hidden"; // Optional: show later via animation
+
+    // Create the spider path
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute("d", `M256,0C114.614,0,0,114.606,0,256c0,141.385,114.614,256,256,256c141.385,0,256-114.615,256-256
+		C512,114.606,397.385,0,256,0z M340.82,147.722l41.069,57.534l-75.74,18.965L340.82,147.722z M256.149,228.399l24.218,26.718
+		c0,0-21.562,6.808-48.734-0.394L256.149,228.399z M159.931,150.326l36.855,75.478l-76.246-16.79L159.931,150.326z M321.366,380.412
+		c-1.206,0.472-2.019,1.407-3.112,2.071l-1.17-14.84c-0.097-1.198-0.726-2.29-1.731-3.041c-1.014-0.761-2.307-1.102-3.61-0.962
+		l-20.679,2.413c-2.517,0.297-4.344,2.342-4.16,4.658l1.87,22.479c-18.144,3.697-38.998,4.064-58.462,1.46
+		c-1.363-9.203-2.21-22.462-2.21-22.462c-0.297-2.133-2.256-3.74-4.598-3.776l-20.714-0.402c-1.372-0.035-2.683,0.473-3.609,1.399
+		c-0.926,0.944-1.381,2.176-1.232,3.426l-0.882,13.722c-6.171-2.342-11.79-4.807-16.553-8.207
+		c-38.473-27.452-59.842-68.048-59.152-117.735c10.331,13.267,39.382,22.968,61.8,29.366l0.35,24.052
+		c0.026,2.342,1.904,4.282,4.457,4.492l22.881,1.932c1.373,0.106,2.736-0.305,3.776-1.127c1.031-0.848,1.643-2.028,1.687-3.304
+		l0.699-18.904c13.774,1.958,27.137,2.824,38.56,2.648c11.266-0.149,23.624-1.18,36.253-3.26l1.163,18.397
+		c0.087,1.276,0.717,2.448,1.765,3.252c1.058,0.804,2.404,1.188,3.785,1.031l22.706-2.586c2.526-0.28,4.266-2.272,4.239-4.614
+		l-0.341-23.615c22.218-6.826,49.215-16.982,63.382-31.359C391.984,310.301,364.217,363.702,321.366,380.412z`);
+
+    this.setRandomFill(path, ['#BF6C00', '#BF5300', '#D68120']);
+    this.setRandomOpacity(path, 0.6, 1);
+    this.setRandomScale(path, 0.8, 1.8);
+
+    // Append the path to the <g>
+    falling.appendChild(path);
+    return falling;
+  }
 
   createFallingHalloween() {
     let falling = document.createElementNS("http://www.w3.org/2000/svg", "g");
