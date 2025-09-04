70c50d94a6a560d10834a3033ec5f58a8283593b
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2025-09-04 22:14:59 +0200

    Add air-mouse capabilities

--- client/web/backgrounds/falling-background.js
+++ client/web/backgrounds/falling-background.js
@@ -1,22 +1,86 @@
 export class FallingBackground extends HTMLElement {
 
+  // private properties
+  _config;
+  _hass;
+  _elements = {};
+  _logger;
+  _eventManager;
+  _layoutManager;
+  _resourceManager;
+
   _maxFallings = 30;
+  _resizeObserver;
+  _configChangeRequested = false;
+  _screenWidth;
+  _screenHeight;
 
   constructor() {
     super();
-    this.attachShadow({ mode: 'open' });
-    this._resizeObserver = null;
-    this._fallings = [];
-    this._animations = [];
-    this._configChangeRequested = false;
+
+    this._logger = new Logger(this, "falling-background.js");
+    this._eventManager = new EventManager(this);
+    this._layoutManager = new LayoutManager(this, {});
+    this._resourceManager = new ResourceManager(this, import.meta.url);
+
+    this.doCard();
+    this.doStyle();
+    this.doAttach();
+    this.doQueryElements();
+    this.doListen();
+
+    this.doUpdateLayout();
+    this.doUpdateAddons();
+  }
+
+  getLogger() {
+    return this._logger;
+  }
+
+  setConfig(config) {
+    this._config = config;
+    if (this.getLogger().isDebugEnabled()) console.debug(...this.getLogger().debug("set setConfig(config):", config));
+    if (this.getLogger().isDebugEnabled()) this.getLogger().doLogOnError(this.doSetConfig.bind(this)); else this.doSetConfig();
+  }
+  doSetConfig() {
+    this.doCheckConfig();
+    this.doUpdateConfig();
+  }
+
+  set hass(hass) {
+    if (this.getLogger().isDebugEnabled()) console.debug(...this.getLogger().debug("set hass(hass):", hass));
+    this._hass = hass;
   }
 
   connectedCallback() {
-    const shadow = this.shadowRoot;
+    if (this.getLogger().isDebugEnabled()) console.debug(...this.getLogger().debug("connectedCallback()"));
+    this.doUpdateLayout();
+  }
 
-    // Create styles
-    const style = document.createElement('style');
-    style.textContent = `
+  disconnectedCallback() {
+    if (this.getLogger().isDebugEnabled()) console.debug(...this.getLogger().debug("disconnectedCallback()"));
+    if (this._resizeObserver) {
+      this._resizeObserver.disconnect();
+    }
+  }
+
+  adoptedCallback() {
+    if (this.getLogger().isDebugEnabled()) console.debug(...this.getLogger().debug("adoptedCallback()"));
+  }
+
+  // jobs
+  doCheckConfig() {
+  }
+
+  doCard() {
+    // Create SVG background
+    this._elements.svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    this._elements.svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+  }
+
+  doStyle() {
+    this._elements.style = document.createElement("style");
+    this._elements.style.textContent = `
       :host {
         position: absolute;
         inset: 0;
@@ -37,72 +101,79 @@ export class FallingBackground extends HTMLElement {
         will-change: transform;
       }
     `;
+  }
 
-    // Create SVG
-    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-    shadow.appendChild(style);
-    shadow.appendChild(svg);
-    this._svg = svg;
+  doAttach() {
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.append(this._elements.style, this._elements.svg);
+  }
 
+  doQueryElements() {
+    // Nothing to do
+  }
+
+  doListen() {
     // Observe size changes
-    this._resizeObserver = new ResizeObserver(() => this.onResize());
+    this._resizeObserver = new ResizeObserver(this.onResize.bind(this));
     this._resizeObserver.observe(this);
-    
-    // Wait for layout before creating fallings
-    requestAnimationFrame(() => {
-      const width = this.offsetWidth;
-      const height = this.offsetHeight;
-      if (width && height) {
-        this._svg.setAttribute('viewBox', `0 0 ${width} ${height}`);
-        this._screenWidth = width;
-        this._screenHeight = height;
-        this.createFallings(); // Call it once here
-      }
-    });
   }
 
-  disconnectedCallback() {
-    if (this._resizeObserver) {
-      this._resizeObserver.disconnect();
-    }
-  }
-
-  /**
-   * Call this to change config and restart fallings
-   */
-  async changeConfig() {
-    // Prevent new animations from looping
-    this._configChangeRequested = true;
-
-    // Wait for all animations to finish naturally
-    await Promise.allSettled(this._animations.map(anim => anim.finished));
-
-    // Remove old elements
-    for (const falling of this._fallings) {
-      falling.remove();
-    }
-
-    this._fallings = [];
-    this._animations = [];
-    this._configChangeRequested = false;
-
-    // Start again
-    this.createFallings();
+  doUpdateConfig() {
+    this.doUpdateLayout();
   }
 
   onResize() {
+    this.doResizeBackground();
+  }
+
+  doResizeBackground() {
     const width = this.offsetWidth;
     const height = this.offsetHeight;
+    if (width && height) {
 
-    if (width === 0 || height === 0) return;
+      // Update SVG viewBox
+      this._elements.svg.setAttribute('viewBox', `0 0 ${width} ${height}`);
 
-    // Update SVG viewBox
-    this._svg.setAttribute('viewBox', `0 0 ${width} ${height}`);
+      // Store new dimensions for future animations
+      this._screenWidth = width;
+      this._screenHeight = height;
+      return true;
+    }
+    return false;
+  }
 
-    // Optionally store new dimensions for future animations
-    this._screenWidth = width;
-    this._screenHeight = height;
+  doUpdateLayout() {
+    const fallings = this._elements.fallings;
+    const animations = this._elements.animations;
+    
+    // Lock to prevent new animations from looping
+    this._configChangeRequested = true;
+
+    // Finish all animations now
+    animations.forEach(animation => animation.finish());
+
+    // Remove old fallings from DOM
+    for (const falling of fallings) {
+      falling.remove();
+    }
+
+    // Reset fallings and animations arrays
+    fallings.length = 0;
+    animations.length = 0;
+
+    // Unlock to allow new animations from looping
+    this._configChangeRequested = false;
+
+    // Wait for layout to be ready beofre creating new fallings
+    requestAnimationFrame(this.doCreateFallings.bind(this));
+  }
+
+  doCreateFallings() {
+    // Resize viewbox to parent container dimensions (whenever possible)
+    if (this.doResizeBackground()) {
+      // Create new fallings using current configuration
+      this.createFallings();
+    }
   }
 
   createFallings() {
@@ -110,6 +181,54 @@ export class FallingBackground extends HTMLElement {
       const falling = this.createFalling();
       this.addFalling(falling);
       setTimeout(() => this.animateFalling(falling), this.getBoundRandom(0, 7000));
+    }
+  }
+
+  addFalling(falling) {
+    this._elements.svg.appendChild(falling);
+    this._elements.fallings.push(falling);
+  }
+
+  animateFalling(falling) {
+    const screenWidth = this._screenWidth || this.offsetWidth;
+    const screenHeight = this._screenHeight || this.offsetHeight;
+    if (!screenWidth || !screenHeight) return;
+
+    const startX = this.getBoundRandom(0, screenWidth);
+    const driftX = this.getBoundRandom(-80, 80);
+    const duration = this.getBoundRandom(10000, 20000);
+    const rotateStart = this.getBoundRandom(0, 360);
+    const rotateEnd = rotateStart + this.getBoundRandom(90, 360);
+
+    falling.setAttribute("transform", `translate(${startX}, -60)`);
+
+    const animation = falling.animate([
+      {
+        transform: `translate(${startX}px, -60px) rotate(${rotateStart}deg)`
+      },
+      {
+        transform: `translate(${startX + driftX}px, ${screenHeight + 60}px) rotate(${rotateEnd}deg)`
+      }
+    ], {
+      duration,
+      easing: "ease-in-out"
+    });
+    this._elements.animations.push(animation);
+
+    animation.ready.then(this.onFallingAnimationReady.bind(this, falling));  
+    animation.addEventListener('finish', this.onFallingAnimationFinish.bind(this, falling));
+  }
+
+  onFallingAnimationReady(falling) {
+    falling.style.visibility = 'visible';
+  }
+
+  onFallingAnimationFinish(falling, evt) {
+    const animation = evt.target;
+    animation.cancel();
+    this._elements.animations = this._elements.animations.filter(a => a !== animation);
+    if (!this._configChangeRequested) {
+      this.animateFalling(falling); // loop only if config change not requested
     }
   }
 
@@ -331,54 +450,6 @@ export class FallingBackground extends HTMLElement {
     // Append the path to the <g>
     falling.appendChild(path);
     return falling;
-  }
-
-  addFalling(falling) {
-    this._svg.appendChild(falling);
-    this._fallings.push(falling);
-  }
-
-  animateFalling(falling) {
-    const screenWidth = this._screenWidth || this.offsetWidth;
-    const screenHeight = this._screenHeight || this.offsetHeight;
-    if (!screenWidth || !screenHeight) return;
-
-    const startX = this.getBoundRandom(0, screenWidth);
-    const driftX = this.getBoundRandom(-80, 80);
-    const duration = this.getBoundRandom(10000, 20000);
-    const rotateStart = this.getBoundRandom(0, 360);
-    const rotateEnd = rotateStart + this.getBoundRandom(90, 360);
-
-    falling.setAttribute("transform", `translate(${startX}, -60)`);
-
-    const animation = falling.animate([
-      {
-        transform: `translate(${startX}px, -60px) rotate(${rotateStart}deg)`
-      },
-      {
-        transform: `translate(${startX + driftX}px, ${screenHeight + 60}px) rotate(${rotateEnd}deg)`
-      }
-    ], {
-      duration,
-      easing: "ease-in-out"
-    });
-    this._animations.push(animation);
-
-    animation.ready.then(this.onAnimationReady.bind(this, falling));  
-    animation.addEventListener('finish', this.onAnimationFinish.bind(this, falling));
-  }
-
-  onAnimationReady(falling) {
-    falling.style.visibility = 'visible';
-  }
-
-  onAnimationFinish(falling, evt) {
-    const animation = evt.target;
-    animation.cancel();
-    this._animations = this._animations.filter(a => a !== animation);
-    if (!this._configChangeRequested) {
-      this.animateFalling(falling); // loop only if config change not requested
-    }
   }
 
 }
