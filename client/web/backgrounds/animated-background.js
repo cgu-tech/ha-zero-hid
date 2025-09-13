eb7bcd8c5ce0398860565defbd6410b801322693
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2025-09-13 11:56:23 +0200

    Add animations

--- client/web/backgrounds/animated-background.js
+++ client/web/backgrounds/animated-background.js
@@ -452,11 +452,12 @@ export class AnimatedBackground extends HTMLElement {
 
   findY2(x1, y1, x2, length) {
     const dx = x2 - x1;
-    const dx2 = dx * dx;
+    let dx2 = dx * dx;
     const L2 = length * length;
   
     if (L2 < dx2) {
-      throw new Error(`No real solution: desired length too short for the given x values (should be L2 < dx2, given L2=${L2}, dx2=${dx2}`);
+      if (this.getLogger().isTraceEnabled()) console.debug(...this.getLogger().trace(`No real solution: desired length too short for the given x values (should be L2 < dx2, given L2=${L2}, dx2=${dx2}`));
+      dx2 = L2 - this.getBoundRandom(L2 / 10, L2);
     }
   
     const dy = Math.sqrt(L2 - dx2);
@@ -470,11 +471,12 @@ export class AnimatedBackground extends HTMLElement {
 
   findX2(x1, y1, y2, length) {
     const dy = y2 - y1;
-    const dy2 = dy * dy;
+    let dy2 = dy * dy;
     const L2 = length * length;
   
     if (L2 < dy2) {
-      throw new Error(`No real solution: desired length too short for the given y values (should be L2 < dy2, given L2=${L2}, dy2=${dy2}`);
+      if (this.getLogger().isTraceEnabled()) console.debug(...this.getLogger().trace(`No real solution: desired length too short for the given y values (should be L2 < dy2, given L2=${L2}, dy2=${dy2}`));
+      dy2 = L2 - this.getBoundRandom(L2 / 10, L2);
     }
   
     const dx = Math.sqrt(L2 - dy2);
@@ -523,20 +525,12 @@ export class AnimatedBackground extends HTMLElement {
           const ySwayMin = swings * yDriftMin;
           const ySwayMax = Math.max(ySwayRange.min, ySwayRange.max);
           ySway = this.getBoundRandom(ySwayMin, ySwayMax);
-          if (Math.abs(ySway) > hypothenuse) {
-            const adjustment = this.getBoundRandom(0.01, hypothenuse - 0.01);
-            ySway = ySway > 0 ? hypothenuse - adjustment : -hypothenuse + adjustment;
-          }
           const aux = this.findX2(lastX, lastY, ySway, hypothenuse);
           xSway = auxAxis > 0 ? aux[0] : aux[1];
           if (this.getLogger().isTraceEnabled()) console.debug(...this.getLogger().trace('getStepsSway(...): constrained xSway (auxAxis, aux, xSway, ySway)', auxAxis, aux, xSway, ySway));
         } else {
           const xSwayRange = this.getValidSecondCoordinateRange(lastX, hypothenuse);
           xSway = this.getBoundRandom(xSwayRange.min, xSwayRange.max);
-          if (Math.abs(xSway) > hypothenuse) {
-            const adjustment = this.getBoundRandom(0.01, hypothenuse - 0.01);
-            xSway = xSway > 0 ? hypothenuse - adjustment : -hypothenuse + adjustment;
-          }
           const aux = this.findY2(lastX, lastY, xSway, hypothenuse);
           ySway = Math.max(aux[0], aux[1]);
           if (this.getLogger().isTraceEnabled()) console.debug(...this.getLogger().trace('getStepsSway(...): constrained ySway (auxAxis, aux, xSway, ySway)', auxAxis, aux, xSway, ySway));
