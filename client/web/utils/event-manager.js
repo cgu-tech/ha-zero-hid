387ac90d2929675fe6052b9d1fdb9c9d05ab85af
cgu-tech <3243696+cgu-tech@users.noreply.github.com>
2026-06-02 21:32:34 +0200

    Fix notification callback

--- client/web/utils/event-manager.js
+++ client/web/utils/event-manager.js
@@ -827,13 +827,15 @@ export class EventManager {
       if (this.getLogger().isWarnEnabled()) console.warn(...this.getLogger().warn(`callService(domain, name, args, notifyOnError): undefined hass. Unable to execute the service (called too early before HA hass init or HA unresponsive)`, domain, name, args, notifyOnError));
       return;
     }
-    const promise = hass.callService(domain, name, args, undefined, notifyOnError);
-    if (notifyOnError) {
-      promise.catch((err) => {
-        if (this.getLogger().isWarnEnabled()) console.warn(...this.getLogger().warn(`callService(domain, name, args, notifyOnError): error:`, domain, name, args, notifyOnError, err));
+    return hass.callService(domain, name, args, undefined, notifyOnError).catch((err) => {
 
-        // Check for error and associate a notification message when needed
-        let message = null;
+      // Log raw error
+      if (this.getLogger().isErrorEnabled()) console.error(...this.getLogger().error(`callService(domain, name, args, notifyOnError): raw error:`, domain, name, args, notifyOnError, err));
+
+      // Check for error and associate a notification message when needed
+      if (notifyOnError) {
+
+        // Check HA disconnected error
         if (!hass.connection.connected) {
           message = this._localization.localize("error.ha.connection_lost.message");
         }
@@ -842,13 +844,9 @@ export class EventManager {
         if (message) {
           this.triggerHaosToast(this.getHaElement(), message);
         }
-      });
-    } else {
-      promise.catch((err) => {
-        if (this.getLogger().isWarnEnabled()) console.warn(...this.getLogger().warn(`callService(domain, name, args, notifyOnError): error:`, domain, name, args, notifyOnError, err));
-      });
-    }
-    return promise;
+      }
+
+    });
   }
 
   // Call a command from HAOS custom component 'Globals.COMPONENT_NAME' using WebSockets.
